31a8af6df17b69c6f9d987cfbb8246db1302c65b
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-23 02:06:45 +0600

    Add files via upload

---
+++ number method/number_method.js
@@ -0,0 +1,15 @@
+var num=12334545
+
+console.log(typeof(num));
+
+
+num= toString(num);
+console.log(typeof(num));
+
+
+
+var number= 5.24536565
+
+console.log(number.toFixed(2));
+
+console.log(number.toPrecision(4))
