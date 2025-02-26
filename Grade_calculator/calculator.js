1ae454139223920d57eb58ca244e6553d3739883
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-26 23:35:05 +0600

    Add files via upload

---
+++ Grade_calculator/calculator.js
@@ -0,0 +1,16 @@
+var num = prompt("Enter the number : ");
+//conditional statement
+if(num>=80)
+    document.write("A+");
+else if(num>=70)
+    document.write("A");
+else if(num>=60)
+    document.write("A-");
+else if(num>=50)
+    document.write("B");
+else if(num>=40)
+    document.write("C");
+else if(num>=33)
+    document.write("D");
+else
+document.write("Fail");
