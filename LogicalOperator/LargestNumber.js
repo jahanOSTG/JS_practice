521658b5fa7e9dd5f9e736c00a1539384016cb1c
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-26 23:48:44 +0600

    Add files via upload

---
+++ LogicalOperator/LargestNumber.js
@@ -0,0 +1,10 @@
+var num1 = prompt("Enter the number1 : ");
+var num2 = prompt("Enter the number2 : ");
+var num3 = prompt("Enter the number3 : ");
+
+if(num1>num2 && num1>num3)
+    document.write(" The Largest Number : "+num1);
+else if(num1>num2 && num1>num3)
+    document.write(" The Largest Number : "+num2);
+else
+    document.write(" The Largest Number : "+num3)
