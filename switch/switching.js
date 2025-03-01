0af81bd6a35d4237da5d392462c699406c908556
Faria Jahan Janie <janiefaria315@gmail.com>
2025-03-01 22:03:03 +0600

    Add files via upload

---
+++ switch/switching.js
@@ -0,0 +1,33 @@
+var number = prompt("Enter the number : "); // Fixed the typo
+
+switch (number) {
+    case "0":
+        document.write("Zero");
+        break;
+    case "1":
+        document.write("One");
+        break;
+
+    case "2":
+        document.write("Two");
+        break;
+
+    case "3":
+        document.write("Three");
+        break;
+
+    case "4":
+            document.write("Four");
+            break;
+    
+    case "5":
+            document.write("Five");
+            break;
+    
+    case "6":
+            document.write("Six");
+            break;        
+        
+    default:
+        document.write("Invalid input");
+}
