8acb9749873cb3278533d5ae446859b13ce7da46
Faria Jahan Janie <janiefaria315@gmail.com>
2025-03-13 22:23:50 +0600

    Add files via upload

---
+++ date_object/date.js
@@ -0,0 +1,25 @@
+console.clear();
+
+var date = new Date();
+
+console.log(date);  
+
+var year = date.getFullYear(); 
+console.log(year); 
+
+var month=date.getMonth();
+console.log(month);
+
+var day=date.getDay();
+console.log(day);
+
+var currentDate=date.getDate();
+console.log(currentDate);
+
+
+var currentMinite=date.getMinutes();
+console.log(currentMinite);
+
+
+var currentHour=date.getHours();
+console.log(currentHour);
