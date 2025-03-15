0b1eb2e9cb56bb47208425dcb205b49d965c0220
Faria Jahan Janie <janiefaria315@gmail.com>
2025-03-15 13:10:51 +0600

    Add files via upload

---
+++ OnClick/click.js
@@ -0,0 +1,28 @@
+//image setting
+var img1= document.querySelector("#img1")
+
+function smile(){
+    img1.src="image/happy.jpg";
+
+}
+var img2= document.querySelector("#img2")
+
+function cry(){
+    img1.src="image/cry.jpg";
+
+}
+
+//gif setting
+
+var img3= document.querySelector("#img3")
+
+function on(){
+    img3.src="image/on.webp";
+
+}
+var img4= document.querySelector("#img4")
+
+function off(){
+    img3.src="image/off.webp";
+
+}
