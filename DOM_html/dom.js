4f9a104149cf5eb6ebda518f9abc9c95f788677a
Faria Jahan Janie <janiefaria315@gmail.com>
2025-03-15 11:15:46 +0600

    Add files via upload

---
+++ DOM_html/dom.js
@@ -0,0 +1,12 @@
+//getElementById
+console.clear();
+ var heading1=document.getElementById("first");
+ heading1.innerHTML="Hellow";
+
+ var heading2=document.getElementById("second");
+ heading2.innerHTML="I am fine";
+
+ var paragraph=document.getElementById("para");
+ paragraph.innerHTML="Ok,Buy"
+
+ var heading2
