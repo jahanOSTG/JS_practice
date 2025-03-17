86620ff1da79e8ddbefa501d799424367aca4ad6
Faria Jahan Janie <janiefaria315@gmail.com>
2025-03-17 13:26:37 +0600

    Add files via upload

---
+++ CSS Style Dynamically/index.js
@@ -0,0 +1,11 @@
+var adding= document.querySelector("#style");
+
+function add(){
+    
+    adding.classList.add("stylePara");
+}
+function remove(){
+  
+    adding.classList.remove("stylePara");
+
+}
